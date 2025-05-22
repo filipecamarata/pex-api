50b7199bc7e1f4bd9f6a22f787f11996bfba4849
filipecamarata <107808029+filipecamarata@users.noreply.github.com>
2025-05-22 13:55:51 -0300

    Revert "feat: migração do myql para o postgres"

---
+++ prisma/migrations/20250422074622_update_db/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `roleId` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `Role` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `updatedAt` to the `Category` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE `User` DROP FOREIGN KEY `User_roleId_fkey`;
+
+-- DropIndex
+DROP INDEX `User_roleId_fkey` ON `User`;
+
+-- AlterTable
+ALTER TABLE `Category` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `Product` ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `User` DROP COLUMN `roleId`,
+    ADD COLUMN `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    ADD COLUMN `role` ENUM('USER', 'ADMIN') NOT NULL DEFAULT 'USER',
+    ADD COLUMN `updatedAt` DATETIME(3) NOT NULL;
+
+-- DropTable
+DROP TABLE `Role`;
